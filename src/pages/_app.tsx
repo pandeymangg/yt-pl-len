1a856965a9f6441acb93e48deca86017e584ef81
anshuman9999 <anshuman@tealfeed.com>
2023-02-01 09:46:14 +0530

    feat: changed font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,21 @@ import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
 
+import { Lato } from "@next/font/google";
+
+const lato = Lato({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
